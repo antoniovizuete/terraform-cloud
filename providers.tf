12bcba270264bb71a260ea36b218351a3e6c13c8
Antonio Vizuete <antoniovizuete@gmail.com>
2023-07-09 23:09:40 +0200

    Add AWS credentials vars

--- providers.tf
+++ providers.tf
@@ -14,7 +14,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region     = "eu-west-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
   default_tags {
     tags = var.tags
   }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
 }
+
+
+variable "access_key" {
+
+}
+
+variable "secret_key" {
+
+}
